d6187e136a74b9e5f80eed42e25121a84617026a
alecmerdler <alecmerdler@gmail.com>
2019-04-18 20:26:37 -0400

    refactored DeploymentPodCounts with useSafetyFirst!

--- frontend/public/components/safety-first.tsx
+++ frontend/public/components/safety-first.tsx
@@ -1,6 +1,24 @@
 import * as _ from 'lodash-es';
 import * as React from 'react';
 
+/**
+ * You should pretty much always use this if you are setting React state asynchronously and your component could be unmounted.
+ * (https://github.com/facebook/react/issues/14113)
+ */
+export const useSafetyFirst = <S extends {}>(initialState: S) => {
+  const mounted = React.useRef(true);
+  React.useEffect(() => () => mounted.current = false, []);
+
+  const [value, setValue] = React.useState(initialState);
+  const setValueSafe = (newValue) => {
+    if (mounted.current) {
+      return setValue(newValue);
+    }
+  };
+
+  return [value, setValueSafe] as [S, React.Dispatch<React.SetStateAction<S>>];
+};
+
 // React doesn't like it when you call setState unless your component is mounted
 export class SafetyFirst<props, state> extends React.Component<props, state> {
   /* eslint-disable no-undef */
