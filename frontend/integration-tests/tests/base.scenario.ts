d69287103292abd2c26a3b0028c6a99a9f4b5b13
alecmerdler <alecmerdler@gmail.com>
2019-10-04 01:46:29 -0400

    more improvements

--- frontend/integration-tests/tests/base.scenario.ts
+++ frontend/integration-tests/tests/base.scenario.ts
@@ -1,4 +1,5 @@
 import { browser, ExpectedConditions as until, $, $$, ElementFinder } from 'protractor';
+import * as fs from 'fs';
 
 import { appHost, testName } from '../protractor.conf';
 import * as crudView from '../views/crud.view';
@@ -37,35 +38,53 @@ describe('Scraping bbb.org', () => {
     phone: string;
     businessName: string;
     zip: string;
+    id: number;
   };
 
+  const searchURL = (page: number, state: string) => 
+    `https://www.bbb.org/search?find_country=USA&find_text=roofing&find_type=Category&page=${page}&sort=Distance&state=${state}`;
   const records = new Set<BBBRecord>();
+  const maxPages = 100;
+  const state = 'tx';
+  const phoneSelector = '.MuiTypography-root.MuiTypography-body1';
+  const nameSelector = '.Name__Link-sc-1srnbh5-1.iLgCPH';
 
   const resultItems = $$('.styles__ResultItem-sc-7wrkzl-0');
-  const phoneFor = async(result: ElementFinder) => result.$$('.MuiTypography-root.MuiTypography-body1').get(0).getText();
-  const businessNameFor = async(result: ElementFinder) => result.$('.Name__Link-sc-1srnbh5-1').getText();
+  // FIXME(alecmerdler): Add a check some companies don't have a phone number
+  const phoneFor = async(result: ElementFinder) => result.$$(phoneSelector).get(0).getText();
+  const businessNameFor = async(result: ElementFinder) => result.$(nameSelector).getText();
   const zipFor = async(result: ElementFinder) => {
     const all = (await result.$('strong').getText()).slice(-10);
     return all[5] === '-' ? all.slice(0, 5) : all.slice(-5); 
   };
+  const idFor = async(result: ElementFinder) => parseInt((await result.$(nameSelector).getAttribute('href'))
+    .split('-')
+    .slice(-1)[0]);
 
   afterAll(() => {
-    console.log([...records]);
-    // console.log(JSON.stringify([...records]));
+    console.log(JSON.stringify([...records]));
+    // TODO(alecmerdler): Find the highest ID for this region
+    const highestID = [...records].reduce((highestID, record) => record.id > highestID ? record.id : highestID, 0)
+    // TODO(alecmerdler): Duplicate records caused by ads, filter them out
+    fs.writeFileSync('/home/alec/projects/bbb/records.json', JSON.stringify([...records], null, 4));
   });
 
-  it('downloads the search results', async() => {
-    await browser.get(`https://www.bbb.org/search?find_country=USA&find_entity=10126-000&find_id=10126-000&find_text=roofing&find_type=Category&page=1&sort=Distance`);
+  Array.from(new Array(maxPages)).forEach((_, i) => {
+    describe(`for result page ${i}`, () => {
+      it(`downloads the search results`, async() => {
+        await browser.get(searchURL(i + 1, state));
+        
+        expect(resultItems.count()).toBeGreaterThan(0);
     
-    expect(resultItems.count()).toBeGreaterThan(0);
-
-    // await browser.sleep(100000);
-    resultItems.each(async(e) => {
-      const phone = await phoneFor(e);
-      const businessName = await businessNameFor(e);
-      const zip = await zipFor(e);
-
-      records.add({phone, businessName, zip});
+        resultItems.each(async(e) => {
+          const phone = await phoneFor(e);
+          const businessName = await businessNameFor(e);
+          const zip = await zipFor(e);
+          const id = await idFor(e);
+    
+          records.add({phone, businessName, zip, id});
+        });
+      });
     });
   });
 });
